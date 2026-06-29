a4de264fbfb12b87a405ed247ccb2cca75a40d9f
lastBcell <saahil.sl.arnd@gmail.com>
2026-06-30 02:53:42 +0530

    Changes in the ENV file

--- ADK-Local/my_first_agent/agent.py
+++ ADK-Local/my_first_agent/agent.py
@@ -1,7 +1,13 @@
 from google.adk.agents.llm_agent import Agent
+from dotenv import load_dotenv
+import os
+
+# Load variables from the .env file into the environment
+load_dotenv()
+
 
 root_agent = Agent(
-    model='gemini-2.5-flash',
+    model=os.getenv('MODEL'),
     name='root_agent',
     description='A helpful assistant for user questions.',
     instruction='Answer user questions to the best of your knowledge',
